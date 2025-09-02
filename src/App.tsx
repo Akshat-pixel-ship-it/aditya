b96ee6ffad23bcbefc5fb2cbdc65f5407466225a
Akshat-pixel-ship-it <shubhdubey007sd@gmail.com>
2025-09-02 19:37:19 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Programs from './components/Programs';
+import Courses from './components/Courses';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
+import BackgroundShapes from './components/BackgroundShapes';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white relative overflow-hidden">
+      <BackgroundShapes />
+      <div className="relative z-10">
+        <Header />
+        <Hero />
+        <Programs />
+        <Courses />
+        <Testimonials />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
